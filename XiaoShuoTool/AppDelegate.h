457973298e080da18d43e611900751ed2f1621a7
整吗好医生 <thelastcode@192.168.10.15>
2016-05-25 14:03:00 +0800

    Initial Commit

---
+++ XiaoShuoTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XiaoShuoTool
+//
+//  Created by AnFeng on 16/5/25.
+//  Copyright © 2016年 TheLastCode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XiaoShuoTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XiaoShuoTool
+//
+//  Created by AnFeng on 16/5/25.
+//  Copyright © 2016年 TheLastCode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
